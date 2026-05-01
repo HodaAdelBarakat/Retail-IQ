d42586ae686e4fa89d370cccbbe7fc75764df979
HodaAdel <adelhoda771@gmail.com>
2026-05-01 23:03:54 +0300

    Update report_generator.py

--- report_generator.py
+++ report_generator.py
@@ -152,7 +152,9 @@ def create_pdf_report(kpis, executive_orders_en, financial_impact, anomalies, fo
     # --------------------------------------------------------
     # 4. Administrative Action Plan
     # --------------------------------------------------------
-    pdf.add_page()
+   if pdf.get_y() > 220: 
+        pdf.add_page()
+        
     pdf.section_title("4. Administrative Action Plan")
     
     pdf.set_font("Helvetica", "B", 11)
